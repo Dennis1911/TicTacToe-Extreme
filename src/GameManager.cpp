daa00e9187247896179787a63dde6c656a2509c4
Dennis1911 <52860328+Dennis1911@users.noreply.github.com>
2022-07-29 19:03:58 +0200

    GameManager V2

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -10,7 +10,7 @@ int GameManager::countPlayers()
     int maxPlayers = 10; // temp muss weg
     int playerCount = InputHandler::getIntFromRange("Gebe die Anzahl der Spieler an: ", 1, maxPlayers);// from range weil max players == fields
     system("cls");
-    // int playerCount = 10; // temp
+    
     cout << "Anzahl der Spieler: " << playerCount << endl;
     return playerCount;
 }
@@ -18,18 +18,29 @@ int GameManager::countPlayers()
 // choose how many and which types of players will play
 Modes GameManager::choosePlayers(int playerCount)
 {
-    for (int i = 0; i <= playerCount; i++)
-    {
-        /* code */
-    }
-    
-    return human;
+    // cout << "Who should be " << i << "sein?" << endl;
+        string textRequest = "Who should be player ";// << stoi(i) << "be?"; // stoi aber nach str??
+        string playerModeStr = InputHandler::getString(textRequest);
+        int playerMode = stoi(playerModeStr);
+        Modes mode;
+        if (playerMode == 1)
+        {
+            mode = smartBot;
+        }
+    return mode;
 }
 
 void GameManager::startGame()
 {
     int playerCount = countPlayers();
-    Modes gamemode1 = choosePlayers(playerCount);
+    cout << "Human: 1" << endl;
+    cout << "SmartBot: 2" << endl;
+    cout << "RandomBot: 3" << endl;
+    for (int i = 1; i <= playerCount; i++) // wahrscheinlich for loop auserhalb und so das multiset füllen
+    {
+        Modes gamemode = choosePlayers(playerCount);
+        cout << "Player " << i << " is " << gamemode << endl;
+        //Modes mode = playerMode; // bei enum switch case oder wie kann man aus einer Zahl ein Mode machen??
+        // unordered multiset adden
+    }
 }
-
-// InputManager schreiben... Damit man Spieleranzahl bestimmen kann... danach jeweils pro Spieler abfragen, welcher Typ gewählt wird
